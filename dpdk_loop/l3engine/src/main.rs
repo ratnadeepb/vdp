268d17834fd76494b12540d6a6f0aeb1d7fe9746
ratnadeepb <ratnadeep.bhattacharya1983@gmail.com>
2021-02-09 21:30:42 -0600

    latest

--- dpdk_loop/l3engine/src/main.rs
+++ dpdk_loop/l3engine/src/main.rs
@@ -4,13 +4,128 @@ pub use apis::*;
 
 use crate::{eal_cleanup, eal_init, Mbuf, Mempool, Port};
 use log;
-use std::sync::{
-	atomic::{AtomicBool, Ordering},
-	Arc,
+use std::{
+	net::Ipv4Addr,
+	slice::from_raw_parts_mut,
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		Arc,
+	},
 };
+
+use pnet::{
+	datalink::MacAddr,
+	packet::{
+		arp::{ArpHardwareTypes, ArpOperations, MutableArpPacket},
+		ethernet::{EtherTypes, MutableEthernetPacket},
+		MutablePacket, Packet,
+	},
+};
+
+use etherparse::{InternetSlice, LinkSlice, ReadError, SlicedPacket, TransportSlice};
 
 const G_MEMPOOL_NAME: &str = "GLOBAL_MEMPOOL";
 const QUEUE_SZ: usize = 32;
+
+const ETHERTYPE_ARP: u16 = 0x0806;
+
+struct FiveTuple {
+	src_mac: [u8; 6],
+	src_ip: Ipv4Addr,
+	dst_mac: [u8; 6],
+	dst_ip: Ipv4Addr,
+	ethertype: u16,
+}
+
+pub(crate) fn parse_pkt(pkt: &[u8], local_ip: Ipv4Addr, local_mac: [u8; 6]) -> Option<FiveTuple> {
+	let sliced_pkt = SlicedPacket::from_ethernet(pkt).unwrap();
+	let linkslice = sliced_pkt.link.unwrap();
+	let ipslice = sliced_pkt.ip.unwrap();
+	let transportslice = sliced_pkt.transport.unwrap();
+	let link = match linkslice {
+		LinkSlice::Ethernet2(l) => l.to_header(),
+	};
+	let ip = match ipslice {
+		InternetSlice::Ipv4(ip) => ip,
+		_ => return None,
+	};
+	let transport = match transportslice {
+		TransportSlice::Tcp(tcp) => tcp,
+		TransportSlice::Udp(_) => return None,
+	};
+	let src_mac = link.source;
+	let dst_mac = link.destination;
+	let ethertype = link.ether_type;
+	let src_ip = ip.source_addr();
+	let dst_ip = ip.destination_addr();
+	// let dst_port = transport.destination_port();
+	if dst_ip != local_ip {
+		return None;
+	}
+
+	if dst_mac != local_mac {
+		return None;
+	}
+
+	// if !ports.contains(&dst_port) {
+	// 	return Err(FiveTupleErr::WrongPort);
+	// }
+
+	Some(FiveTuple {
+		src_ip,
+		src_mac,
+		dst_mac,
+		dst_ip,
+		ethertype,
+	})
+}
+
+fn arr_to_macaddr(mac: [u8; 6]) -> MacAddr {
+	MacAddr::new(mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
+}
+
+fn handle_arp(
+	local_ip: Ipv4Addr,
+	local_mac: [u8; 6],
+	dst_mac: [u8; 6],
+	dst_ip: Ipv4Addr,
+	mp: &Mempool,
+	ethertype: u16,
+) -> Option<Mbuf> {
+	if local_mac != dst_mac || local_ip != dst_ip {
+		return None;
+	}
+
+	if ethertype.to_be() == ETHERTYPE_ARP {
+		if local_mac == dst_mac || local_ip == dst_ip {
+			let mut eth_buf = [0u8; 42];
+			let mut eth_pkt = MutableEthernetPacket::new(&mut eth_buf)?;
+			eth_pkt.set_destination(MacAddr::broadcast());
+			let source_mac = arr_to_macaddr(local_mac);
+			eth_pkt.set_source(source_mac);
+			eth_pkt.set_ethertype(EtherTypes::Arp);
+			let mut arp_buffer = [0u8; 28];
+			let mut arp_packet = MutableArpPacket::new(&mut arp_buffer)?;
+			arp_packet.set_hardware_type(ArpHardwareTypes::Ethernet);
+			arp_packet.set_protocol_type(EtherTypes::Ipv4);
+			arp_packet.set_hw_addr_len(6);
+			arp_packet.set_proto_addr_len(4);
+			arp_packet.set_operation(ArpOperations::Request);
+			arp_packet.set_sender_hw_addr(source_mac);
+			arp_packet.set_sender_proto_addr(local_ip);
+			arp_packet.set_target_hw_addr(MacAddr::zero());
+			arp_packet.set_target_proto_addr(dst_ip);
+
+			eth_pkt.set_payload(arp_packet.packet_mut());
+
+			match Mbuf::from_bytes(eth_pkt.packet(), mp) {
+				Ok(out_buf) => return Some(out_buf),
+				Err(_) => {}
+			}
+		}
+	}
+	None
+}
 
 /// Handle Ctrl+C
 fn handle_signal(kr: Arc<AtomicBool>) {
@@ -20,9 +135,15 @@ fn handle_signal(kr: Arc<AtomicBool>) {
 	.expect("Error setting Ctrl-C handler");
 }
 
-fn recv_pkts(port: &Port, in_pkts: &mut Vec<Mbuf>) -> usize {
+fn recv_pkts(port: &[Port; 2], in_pkts: &mut Vec<Mbuf>) -> usize {
 	let queue_id = unsafe { dpdk_sys::_rte_lcore_id() as u16 };
-	in_pkts.extend(port.receive(queue_id, in_pkts.len()));
+	in_pkts.extend(port[0].receive(queue_id, in_pkts.len()));
+	// in_pkts.extend(port[1].receive(queue_id, in_pkts.len()));
+
+	#[cfg(feature = "debug")]
+	if in_pkts.len() > 0 {
+		println!("got {} pkts", in_pkts.len());
+	}
 
 	in_pkts.len()
 }
@@ -71,10 +192,15 @@ fn main() {
 	log::info!("setup ports");
 	#[cfg(feature = "debug")]
 	println!("setup ports");
-	let eth_devs = "port0";
-	let mut port = Port::new(eth_devs, 0u16).unwrap();
-	port.configure(cores.len() as u16, &mempool).unwrap();
-	port.start().unwrap();
+	let eth_dev1 = "port0";
+	let mut port1 = Port::new(eth_dev1, 0u16).unwrap();
+	port1.configure(cores.len() as u16, &mempool).unwrap();
+	port1.start().unwrap();
+	let eth_dev2 = "port1";
+	let mut port2 = Port::new(eth_dev2, 1u16).unwrap();
+	port2.configure(cores.len() as u16, &mempool).unwrap();
+	port2.start().unwrap();
+	let ports = [port1, port2];
 
 	#[cfg(feature = "debug")]
 	println!("ports set");
@@ -91,12 +217,33 @@ fn main() {
 	#[cfg(feature = "debug")]
 	println!("main: secondary started");
 	while keep_running.load(Ordering::SeqCst) {
-		let _rsz = recv_pkts(&port, &mut in_pkts);
+		let rsz = recv_pkts(&ports, &mut in_pkts);
 		#[cfg(feature = "debug")]
-		println!("Received {} packets", _rsz);
-		let _tsz = xmit_pkts(&port, &mut in_pkts);
-		#[cfg(feature = "debug")]
-		println!("Received {} packets", _rsz);
+		if rsz > 0 {
+			println!("Received {} packets", rsz);
+		}
+		if rsz > 0 {
+			let _tsz = xmit_pkts(&port2, &mut in_pkts);
+			#[cfg(feature = "debug")]
+			println!("Transmitted {} packets", _tsz);
+			for pkt in &in_pkts {
+				let local_ip = Ipv4Addr::new(192, 168, 1, 2);
+				let mac = [0x90, 0xe2, 0xba, 0x87, 0x6b, 0xe8];
+				let buf =
+					unsafe { from_raw_parts_mut(dpdk_sys::_pkt_raw_addr(pkt.get_ptr()), 1536) };
+				let ft = parse_pkt(buf, local_ip, mac).unwrap();
+				let mut mbuf = vec![handle_arp(
+					local_ip,
+					mac,
+					ft.src_mac,
+					ft.src_ip,
+					&mempool,
+					ft.ethertype,
+				)
+				.unwrap()];
+				xmit_pkts(&port1, &mut mbuf);
+			}
+		}
 	}
 
 	#[cfg(feature = "debug")]
